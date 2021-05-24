a1e9ff247f4ff3878a4fb75eda5371ab079c189d
Pablo <pablochavezing@gmail.com>
2021-05-23 17:07:46 -0700

    Actualizacion de vistas

--- db/migrate/20210523233240_create_books.rb
+++ db/migrate/20210523233240_create_books.rb
@@ -3,7 +3,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
     create_table :books do |t|
       t.string :title
       t.string :author
-      t.boolean :state
+      t.string :state
       t.date :date1
       t.date :date2
 
